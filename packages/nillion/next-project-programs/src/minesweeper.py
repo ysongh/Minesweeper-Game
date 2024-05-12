352ab244f40c435649bd836648aeb4e3141431b1
You Song Hou <ysongweb3@gmail.com>
2024-05-11 21:16:16 -0400

    feat: add a sequence example

--- packages/nillion/next-project-programs/src/minesweeper.py
+++ packages/nillion/next-project-programs/src/minesweeper.py
@@ -5,6 +5,7 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = my_int1 - my_int2
+    grid: list[SecretInteger] = []
+    grid.append(my_int1 - my_int2)
 
-    return [Output(new_int, "my_output", party1)]
+    return [Output(grid[0], "my_output", party1)]
