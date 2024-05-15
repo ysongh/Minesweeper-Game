8581fa328dbad2498c921665e0658307f941e83c
You Song Hou <ysongweb3@gmail.com>
2024-05-15 00:20:51 -0400

    refactor: change the way to populate the grid list

--- packages/nextjs/public/programs/minesweeper.py
+++ packages/nextjs/public/programs/minesweeper.py
@@ -5,12 +5,14 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=miner))
     my_int2 = SecretInteger(Input(name="my_int2", party=miner))
 
-    grid = [
-        [
-            Integer(0) for i in range(3)
-        ]
-        for j in range(3)    
-    ]
+    grid = []
+    index = 0
+
+    for r in range(3):
+        temp = []
+        for c in range(3):
+            temp.append(Integer(0))
+        grid.append(temp)
 
     grid[0][0] = my_int1 + my_int2
 
