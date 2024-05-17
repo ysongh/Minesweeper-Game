e4574275e51d4a45eced85ccf0abeb9570877506
You Song Hou <ysongweb3@gmail.com>
2024-05-16 21:04:03 -0400

    feat: add checks to see if the element has 99

--- packages/nextjs/public/programs/minesweeper.py
+++ packages/nextjs/public/programs/minesweeper.py
@@ -2,6 +2,7 @@ from nada_dsl import *
 
 def nada_main():
     miner = Party(name="Miner")
+    # minesweeper = Party(name="Minesweeper")
     my_int1 = SecretInteger(Input(name="my_int1", party=miner))
     my_int2 = SecretInteger(Input(name="my_int2", party=miner))
 
@@ -17,6 +18,20 @@ def nada_main():
             index += Integer(1)
             
         grid.append(temp)
+
+
+    count = Integer(0)
+
+    ## Up
+    count += (grid[0][1] >= Integer(99)).if_else((grid[0][1] <= Integer(99)).if_else(Integer(1), Integer(0)), Integer(0))
+    ## Down
+    count += (grid[2][1] >= Integer(99)).if_else((grid[2][1] <= Integer(99)).if_else(Integer(1), Integer(0)), Integer(0))
+    ## Left
+    count += (grid[1][0] >= Integer(99)).if_else((grid[1][0] <= Integer(99)).if_else(Integer(1), Integer(0)), Integer(0))
+    ## Right
+    count += (grid[1][2] >= Integer(99)).if_else((grid[1][2] <= Integer(99)).if_else(Integer(1), Integer(0)), Integer(0))
+
+    grid[1][1] = count
 
     outputs: list[Output] = []
 
